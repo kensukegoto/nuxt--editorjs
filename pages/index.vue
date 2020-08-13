658aa2d8a35400f37cc7bc0c87ed6151d4266a5a
kensuke goto <kensuke.goto.1981@gmail.com>
2020-08-13 16:32:56 +0900

    追加：テキストエリアを並べる

--- pages/index.vue
+++ pages/index.vue
@@ -1,52 +1,55 @@
 <template>
-  <div>
-    <div>
-      <div>記事作成</div>
-      <div >
-        <client-only placeholder="loading">
-          <!-- <Editor /> -->
-        </client-only>
-        <!-- <div id="editorjs"/> -->
+  <section class="editor">
+    <div class="editor__item item">
+      <div class="item__header">
+        <p>×</p>
+      </div>
+      <div class="item__body">
+        <textarea name="" id=""></textarea>
       </div>
     </div>
-  </div>
+    <div class="editor__item">
+      <div class="item__header">
+        <p>×</p>
+      </div>
+      <div class="item__body">
+        <textarea name="" id=""></textarea>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-import Editor from "~/components/Editor/index"
 
-  export default {
-    name: "ArticleCreateForm",
-    data: () => ({
-      config: {},
-      editor: {},
-    }),
-    components:{
-      Editor
-    },
-    mounted() {
-
-      console.log(this)
-
-      // this.editor = new EditorJS({
-      //   holder: 'editorjs', 
-      // });
-
-    },
-    methods: {
-      save() {
-        console.log("保存をご希望ですか？");
-        // this.editor.save().then((data) => {
-        //   console.log(data);
-        //   this.article = data
-        // }).catch(err => {
-        //   console.log(err)
-        // })
-      },
-    }
-  }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.editor{
 
+  width: 800px;
+  margin: 0 auto;
+
+  &__item{
+    margin-top: 24px;
+    padding-top: 32px;
+  }
+
+  .item{
+    &__header{
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+      height: 32px;
+      background: #88684e;
+      p{
+        margin-right: 24px;
+      }
+    }
+  }
+  textarea{
+    width: 100%;
+    min-height: 200px;
+    border: none;
+  }
+}
 </style>
